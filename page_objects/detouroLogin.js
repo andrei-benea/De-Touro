9d66dd3083ca199038a01984edbf1228bfe96b0d
andrei.benea <andrei.benea@p4b.ro>
2022-03-18 13:39:54 +0100

    Added custom commands!

--- page_objects/detouroLogin.js
+++ page_objects/detouroLogin.js
@@ -8,6 +8,19 @@ module.exports = {
     },
 
     commands:[{
-
+        setUser(){
+            const username = 'andrei.benea89+989@gmail.com'
+            return this
+                .setValue('@usernameInput', username);
+        },
+        setPass(){
+            const password = 'ungam+20'
+            return this
+                .setValue('@passwordInput', password);
+        },
+        login(){
+            return this
+                .click('@submitButton');
+        }
     }]
 }
--- tests/login.js
+++ tests/login.js
@@ -3,16 +3,16 @@ module.exports = {
 
     'Login into De-Touro QA'(browser){
         
-        const username = "andrei.benea89+989@gmail.com"
-        const password = "ungam+20"
+      //  const username = 'andrei.benea89+989@gmail.com';
+      //  const password = 'ungam+20';
 
         const login = browser.page.detouroLogin();
 
         login
             .navigate()
-            .setValue('@usernameInput', username)
-            .setValue('@passwordInput', password)
-            .click('@submitButton')
+            .setUser()
+            .setPass()
+            .login()
             .assert.urlContains('https://qa.de-touro.de/Index')
     }
 }
