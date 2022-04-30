fbfaa98325f9f06e7d4bf59d54745cd3ef76efa5
Andrei Home <andrei.benea@p4b.ro>
2022-05-01 01:00:27 +0300

    async login into dt le

---
+++ page_objects/asyncDtIndex.js
@@ -0,0 +1,8 @@
+import { patient } from "../tests_input/patientData";
+
+export default class AsyncDtIndex {
+    url = 'https://qa.de-touro.de/Index';
+    elements = {
+
+    }
+};
---
+++ tests_input/loginInfoLe.js
@@ -0,0 +1,6 @@
+module.exports = {
+    credentials: {
+        username: 'andrei.benea89+989@gmail.com',
+        password: 'ungam+20'
+    }
+}
