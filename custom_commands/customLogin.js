4e3494e2959f04e1893068b9314c8196b0a8c6e3
Andrei Home <andrei.benea@p4b.ro>
2022-04-30 20:08:24 +0300

    working custom login command

---
+++ custom_commands/customLogin.js
@@ -0,0 +1,16 @@
+import { credentials } from "../tests_input/loginInfoKt";
+
+module.exports = class CustomLogin {
+    async command(user, pass, button) {
+        return browser
+            .setValue(user, credentials.username, async () => {
+                console.log('Setting username..')
+            })
+            .setValue(pass, credentials.password, async () => {
+                console.log('Setting password..')
+            })
+            .click(button, async () => {
+                console.log('Logging in...')
+            })
+    }
+}
--- page_objects/asyncKkx3.js
+++ page_objects/asyncKkx3.js
@@ -1,16 +1,9 @@
-import { credentials } from "../tests_input/loginInfoKt";
-// import CustomClick from "../custom_commands/customClick";
-
-// let customClick = new CustomClick();
-
-// const customClick = new CustomClick();
-
 export default class AsyncKkx3 {
     url = 'https://check-kkx3.zhp-online.de/x3/de/';
     elements = {
         usernameInput: '#ext-comp-1020',
         passwordInput: '#ext-comp-1021',
-        submitButton: '#ext-gen48',
+        loginButton: '#ext-gen48',
         kkSwitchButton: '#ext-gen136',
         selectGroupInput: '#ext-comp-1037',
         kkDeTouroGroup: '#ext-comp-1033 > div > div:nth-child(33)',
@@ -32,19 +25,9 @@ export default class AsyncKkx3 {
     }
     async loginKt() {
         return browser
-            .setValue(this.elements.usernameInput, credentials.username, async () => {
-                console.log('Setting username..')
-            })
-            .setValue(this.elements.passwordInput, credentials.password, async () => {
-                console.log('Setting password..')
-            })
-            .click(this.elements.submitButton, async () => {
-                console.log('Logging in...')
-            })
-            .waitForElementVisible(this.elements.kkSwitchButton, 'Switching to KK group..')
-            .click(this.elements.kkSwitchButton)
-            .waitForElementVisible(this.elements.kkDeTouroGroup, 'Selecting De-Touro group..')
-            .click(this.elements.kkDeTouroGroup)
+            .customLogin(this.elements.usernameInput, this.elements.passwordInput, this.elements.loginButton)
+            .customClick(this.elements.kkSwitchButton)
+            .customClick(this.elements.kkDeTouroGroup)
             .waitForElementVisible(this.elements.loggedInUser, 'Login successful!')
             .expect.element(this.elements.loggedInUser).text.to.contain('Andrei Benea')
     };
