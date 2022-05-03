dc6f43fa7dc2922c0009871098ddd780ae2e1bf9
Andrei Home <andrei.benea@p4b.ro>
2022-05-04 00:58:32 +0300

    not working but almost almost

--- custom_commands/smartKtas.js
+++ custom_commands/smartKtas.js
@@ -1,9 +1,30 @@
 module.exports = class SmartKtas {
     async command(element) {
-        return browser
+        // for (let i = 0; i < 1; i++) {
+        await browser
             .waitForElementVisible(element, 'Element visible!')
-            .getElementProperty(element, 'id', async (result) => {
+            .elements('css selector', element, function (elements) {
+                elements.value.forEach(function (elementsObj) {
+                    let elementID = elementsObj[Object.keys(elementsObj)[0]]
+                    console.log(`WebElementID: ${elementID}`)
+                    browser.elementIdAttribute(elementID, function (result) {
+                        // if (data-recordindex == 0) {
+                        console.log('\n' + result.value)
+                        // }
+                    })
+                })
+                // console.log(result.value)
+            })
+            .getAttribute(element, 'data-recordindex', async (result) => {
+                for (let i = 0; i <= result.value; i++) {
+                    console.log('inner loop')
+                }
                 console.log(result)
             })
+        // .expect.element(element).to.have.attribute('data-recordindex')
+        // .equals('0');
+        console.log('loop')
+        // }
+        return
     }
 }
--- page_objects/asyncDtIndex.js
+++ page_objects/asyncDtIndex.js
@@ -2,14 +2,15 @@ export default class AsyncDtIndex {
     url = 'https://qa.de-touro.de/Index';
     elements = {
         ktaFirstRow: '[data-recordindex="0"]',
-        ktaGrid: '[id="gridview-1267-body"]',
-        ktaGrid2: '[class="x-panel x-grid-with-col-lines x-grid-no-row-lines x-fit-item x-panel-default x-grid"]:nth-child(2) > div:nth-child(2) > div > table > tbody',
+        ktaGridBad: '[id="gridview-1250-body"]',
+        ktaGrid: '[class="x-gridview-1250-table x-grid-table x-grid-with-col-lines"]',
         ktaGridRows: '[class="x-panel x-grid-with-col-lines x-grid-no-row-lines x-fit-item x-panel-default x-grid"]:nth-child(2) > div:nth-child(2) > div > table > tbody > tr',
+        ktaGridRow: '[class="x-grid-row tour-grid-unread-row x-grid-data-row"]',
+        ktaGridRowAlt: '[class="x-grid-row x-grid-row-alt tour-grid-unread-row x-grid-data-row"]',
         ukfDecideLaterButton: '[id="button-1021-btnEl"]',
     }
     async smartKtas() {
         return browser
-            // .customClick(this.elements.ukfDecideLaterButton)
-            .smartKtas(this.elements.ktaGridRows)
+            .smartKtas(this.elements.ktaGridRow)
     }
 };
