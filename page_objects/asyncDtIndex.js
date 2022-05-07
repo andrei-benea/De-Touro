b142f5141b3ea955a1fcf2b6a382ad407e68f676
Andrei Benea <andrei.benea89@gmail.com>
2022-05-07 03:53:13 +0300

    lan

--- page_objects/asyncDtIndex.js
+++ page_objects/asyncDtIndex.js
@@ -2,14 +2,20 @@ export default class AsyncDtIndex {
     url = 'https://qa.de-touro.de/Index';
     elements = {
         ktaGridBody: '[class="x-panel-body x-panel-body-default x-layout-fit x-panel-body-default x-docked-noborder-top x-docked-noborder-right x-docked-noborder-bottom x-docked-noborder-left"]',
+        ktaGridRowReadBidField: '[class="x-grid-row x-grid-data-row"] > td:nth-child(15)',
         ktaGridRowUnread: '[class="x-grid-row tour-grid-unread-row x-grid-data-row"]',
         ktaGridRowUnreadBidField: '[class="x-grid-row tour-grid-unread-row x-grid-data-row"] > td:nth-child(15)',
+        ktaGridRowAltReadBidField: '[class="x-grid-row x-grid-row-alt x-grid-data-row"] > td:nth-child(15)',
         ktaGridRowAltUnread: '[class="x-grid-row x-grid-row-alt tour-grid-unread-row x-grid-data-row"]',
         ktaGridRowAltUnreadBidField: '[class="x-grid-row x-grid-row-alt tour-grid-unread-row x-grid-data-row"] > td:nth-child(15)',
-        ktaDetailsContainer: '[class="x-window x-layer x-window-default x-closable x-window-closable x-window-default-closable x-border-box x-resizable x-window-resizable x-window-default-resizable"]'
+        ktaDetailsContainer: '[class="x-window x-layer x-window-default x-closable x-window-closable x-window-default-closable x-border-box x-resizable x-window-resizable x-window-default-resizable"]',
+        ktaDetailsContainerTitle: '[class="x-window x-layer x-window-default x-closable x-window-closable x-window-default-closable x-border-box x-resizable x-window-resizable x-window-default-resizable"] [class="x-panel-body x-panel-body-default x-box-layout-ct x-panel-body-default x-docked-noborder-top x-docked-noborder-right x-docked-noborder-bottom x-docked-noborder-left"] > div > div',
+        ktaDetailsPlaceBidButtonKta: '[class="x-toolbar x-docked x-toolbar-footer x-docked-bottom x-toolbar-docked-bottom x-toolbar-footer-docked-bottom x-box-layout-ct"] > div > div > a:nth-child(3)',
+        ktaDetailsPlaceBidButtonDa: '[class="x-toolbar x-docked x-toolbar-footer x-docked-bottom x-toolbar-docked-bottom x-toolbar-footer-docked-bottom x-box-layout-ct"] > div > div > a:nth-child(4)',
+        ktaDetailsPlaceBidButtonBa: '[class="x-toolbar x-docked x-toolbar-footer x-docked-bottom x-toolbar-docked-bottom x-toolbar-footer-docked-bottom x-box-layout-ct"] > div > div > a:nth-child(4)',
     }
     async smartKtas() {
         return browser
-            .smartKtas(this.elements.ktaGridRowUnread, this.elements.ktaGridRowAltUnread, this.elements.ktaDetailsContainer)
+            .smartKtas(this.elements.ktaGridRowUnread, this.elements.ktaGridRowAltUnread, this.elements.ktaDetailsContainer, this.elements.ktaDetailsContainerTitle, this.elements.ktaGridRowReadBidField, this.elements.ktaGridRowAltReadBidField)
         }
 };
