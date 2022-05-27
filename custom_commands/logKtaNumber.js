b83c5204bacefb4ace7bf89925cd9df586ca09a7
andrei.benea <andrei.benea@p4b.ro>
2022-05-27 11:46:21 +0300

    refactor log kta number

--- custom_commands/logKtaNumber.js
+++ custom_commands/logKtaNumber.js
@@ -1,14 +1,28 @@
 module.exports = class LogKtaNumber {
     async command(element) {
         var fs = require('fs');
-        const result = await
-            browser
-                .waitForElementVisible(element, 'Element visible!')
-                .getText(element);
-        console.log('The KTA Nr. is:', result);
-        fs.writeFile('tests_output/ktanumber.txt', result, function (err) {
-            if (err) throw err;
-            console.log('KTA Number saved to output file!')
-        })
+        return browser
+            .waitForElementVisible(element, 'Element visible')
+            .getText(element, (result) => {
+                fs.writeFile('tests_output/ktanumber.txt', result.value, (err) => {
+                    if (err) throw err;
+                    console.log('Saving KTA number to output file!')
+                })
+                console.log('The KTA Nr. is........ ', result.value)
+            })
     }
 }
+// module.exports = class LogKtaNumber {
+//     async command(element) {
+//         var fs = require('fs');
+//         const result = await browser
+//             .waitForElementVisible(element, 'Element visible!')
+//             .getText(element);
+
+//         fs.writeFile('tests_output/ktanumber.txt', result, (err) => {
+//             if (err) throw err;
+//             console.log('KTA Number saved to output file!')
+//         })
+//         console.log('The KTA Nr. is:', result);
+//     }
+// }
