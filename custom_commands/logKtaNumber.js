a6adb940bdaa475fc355c135a11cb794140d7f13
andrei.benea <andrei.benea@p4b.ro>
2022-06-14 15:31:00 +0300

    clean logKtaNumber command as JSON

--- custom_commands/logKtaNumber.js
+++ custom_commands/logKtaNumber.js
@@ -4,26 +4,18 @@ module.exports = class LogKtaNumber {
         return browser
             .waitForElementVisible(element, 'Element visible')
             .getText(element, async (result) => {
-                var json = JSON.stringify(result.value)
+                var testData = {
+                    kta: {
+                        number: result.value,
+                        type: '',
+                    }
+                };
+                var json = JSON.stringify(testData)
                 fs.writeFile('tests_output/ktanumber.json', json, 'utf8', (err) => {
                     if (err) throw err;
-                    console.log('Saving KTA number to output file!')
+                    console.log('Saving KTA number to output JSON!')
                 })
                 console.log('The KTA Nr. is........ ', result.value)
             })
     }
 }
-// module.exports = class LogKtaNumber {
-//     async command(element) {
-//         var fs = require('fs');
-//         const result = await browser
-//             .waitForElementVisible(element, 'Element visible!')
-//             .getText(element);
-
-//         fs.writeFile('tests_output/ktanumber.txt', result, (err) => {
-//             if (err) throw err;
-//             console.log('KTA Number saved to output file!')
-//         })
-//         console.log('The KTA Nr. is:', result);
-//     }
-// }
