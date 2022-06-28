88494c41ee39a2d99a9b9e4d5667d6f25b9a60db
andrei.benea <andrei.benea@p4b.ro>
2022-06-28 13:10:34 +0300

    refactor refresh loop for better readability

--- custom_commands/customRefresh.js
+++ custom_commands/customRefresh.js
@@ -1,46 +1,26 @@
 module.exports = class CustomRefresh {
     async command(button, text) {
-        let rezz = await browser.getText(text, async (result) => {
-            console.log('inside rezz function ' + result.value)
+        let refresh = await browser.getText(text, async (result) => {
             return result.value;
         });
-        console.log('outside first ' + rezz == 'Keine KTA gefunden')
-        while (rezz == 'Keine KTA gefunden') {
+        if (refresh == 'Keine KTA gefunden') {
+            console.log('KTA not present..Starting refresh loop...')
+        }
+        else {
+            console.log('KTA identified!')
+        }
+        while (refresh == 'Keine KTA gefunden') {
             console.count('Attempt: ')
             browser
                 .customClick(button)
                 .pause(5000)
-            let rezzNew = await browser.getText(text, async (result) => {
-                console.log('inside rezzNew function ' + result.value)
+            let fresh = await browser.getText(text, async (result) => {
                 return result.value;
             });
-            console.log('outside last ' + rezz)
-            if (rezzNew != 'Keine KTA gefunden') {
+            if (fresh != 'Keine KTA gefunden') {
+                console.log('KTA identified! Quitting refresh loop...')
                 break;
             }
         }
     }
 }
-///////////////////////////////////////////////////////////////////////////////////////
-////////////////////WORKING AWAIT STRUCTURE, NOT WORKING LOOP!!!!!!!!!!////////////////
-///////////////////////////////////////////////////////////////////////////////////////
-// module.exports = class CustomRefresh {
-//     async command(button, text) {
-//         let rezz = await browser.getText(text, async (result) => {
-//             console.log('inside let ' + result.value)
-//             return result.value;
-//         });
-//         console.log('outside first ' + rezz == 'Keine KTA gefunden')
-//         do {
-//             browser
-//                 .customClick(button)
-//                 .pause(5000)
-//             console.count('Attempt: ')
-//         }
-//         while (rezz == 'Keine KTA gefunden')
-//         console.log('outside second ' + rezz)
-//     }
-// }
-//////////////////////////////////THE END//////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////
